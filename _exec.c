db33f474309ebac3d3429b28157752548a47a34c
AlshimaaAhmed <alshimaa185@gmail.com>
2023-11-17 17:10:59 +0200

    new

--- _exec.c
+++ _exec.c
@@ -5,48 +5,44 @@
  * @comm_path[]:the string that will save the full path of the command
  * @args[]:the secound argument of exeve function
 */
-void _exec(const char *command)
-{
-	char *command_path;
-	char *token, *args[1024];
-	int argc = 0;
-	command_path = malloc(1024);
-	if (command_path == NULL)
-	{
-		write(1,"malloc failed", sizeof("malloc failed"));
-	}
-	/*Tokenize the user input into command and arguments*/
-	token = strtok((char *)command, " ");
-	while (token != NULL)
-	{
-		args[argc] = token;
-		token = strtok(NULL, " ");
-		argc++;
-	}
-	args[argc] = NULL;
-	printf("%s\n", args[0]);
-	find_path(args[0], command_path);
-	if (command_path != NULL)
-	{
-		pid_t child_pid = fork();
+void _exec(const char *command) {
+    char *command_path;
+    char *token, *args[1024];
+    int argc = 0;
+    command_path = malloc(1024);
+    if (command_path == NULL) {
+        write(1, "malloc failed", strlen("malloc failed"));
+        exit(EXIT_FAILURE);
+    }
 
-		if (child_pid == -1)
-		{
-		perror("fork"), exit(EXIT_FAILURE);
-		}
-		else if (child_pid == 0)
-		{
-		if (execve(command_path, args, NULL) == -1)
-		{
-		perror("execve"), exit(EXIT_FAILURE);
-		}
-		}
-		else
-			wait(NULL);
-	}
-	else
-	{
-		perror("stat"), exit(EXIT_FAILURE);
-	}
-	free(command_path);
+    // Tokenize the user input into command and arguments
+token = strtok((char *)command, " ");
+while (token != NULL && argc < 1023) {
+    args[argc] = token;
+    token = strtok(NULL, " ");
+    argc++;
+}
+    args[argc] = NULL;
+    printf("%s\n", args[0]);
+    find_path(args[0], command_path);
+    if (command_path[0] != '\0') {
+        pid_t child_pid = fork();
+
+        if (child_pid == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        } else if (child_pid == 0) {
+            args[0]=command_path;
+            if (execve(command_path, args, NULL) == -1) {
+                perror("execve");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            wait(NULL);
+        }
+    } else {
+        perror("Command not found");
+        exit(EXIT_FAILURE);
+    }
+    free(command_path);
 }
--- path.c
+++ path.c
@@ -4,60 +4,57 @@
  * @command: order name
  * 
 */
-void find_path (char *name, char *command_path)
-{
-	char *full_path,  *copied_full_path;
-	char *path_arry[1024], *token;
-	int i ;
-	struct stat st;
+void find_path(char *name, char *command_path) {
+ char *full_path, *copied_full_path;
+    char *path_arry[1024], *token;
+    int i;
+    struct stat st;
 
-	copied_full_path = malloc(1024);
-	if (copied_full_path == NULL)
-	{
-		write(1,"malloc failed", sizeof("malloc failed"));
-	}
-	
-	/*gits path into array*/
-	for ( i = 0; environ[i]; i++)
-	{
-		if (strncmp(environ[i], "PATH=", 5) == 0)
-			{
-				full_path = *environ + 5;
-				break;
-			}
+    copied_full_path = malloc(1024);  // Adjust the size based on your needs
+    if (copied_full_path == NULL) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
-	}
-	printf ("full path : %s", full_path);
-	strcpy(copied_full_path, full_path);
-	printf (" copied full path : %s", copied_full_path);
-	token = strtok(copied_full_path, ":");
-	i = 0;
-	while(token != NULL)
-	{
-		path_arry[i] = token;
-		token = strtok(NULL, ":");
-		i++;
-	}
-	path_arry[i] = NULL;
-	
-	i = 0;
-	while (path_arry[i])
-	{
-		if (chdir(path_arry[i]) == 0)
-		{
-			if (stat(name,&st) == 0)
-			{
-				printf("check stat\n");
-				strcpy(command_path, path_arry[i]);
-				strcat(command_path, "/");
-				strcat(command_path, name);
-				printf("check command %s\n", command_path);
-				return;
+    // Gets path into array
+    for (i = 0; environ[i]; i++) {
+        if (strncmp(environ[i], "PATH=", 5) == 0) {
+            full_path = environ[i] + 5;  // Skip the "PATH=" prefix
+            break;
+        }
+    }
 
-			}
+   if (full_path[0] == '=') {
+        strcpy(copied_full_path, full_path + 1);  // Copy everything after '='
+    } else {
+        strcpy(copied_full_path, full_path);  // Copy the original string if '=' is not present
+    }
 
-		}
-	}
-	command_path = NULL;
-	free(copied_full_path);
+    //printf("full path : %s\n", full_path);
+    //printf("copied full path : %s\n", copied_full_path);
+
+    token = strtok(copied_full_path, ":");
+    i = 0;
+    while (token != NULL) {
+        path_arry[i] = token;
+        token = strtok(NULL, ":");
+        i++;
+    }
+    path_arry[i] = NULL;
+
+    for (i = 0; path_arry[i]; i++) {
+        strcpy(command_path, path_arry[i]);
+        strcat(command_path, "/");
+        strcat(command_path, name);
+
+        if (stat(command_path, &st) == 0) {
+           // printf("check stat\n");
+           // printf("check command %s\n", command_path);
+            free(copied_full_path);
+            return;
+        }
+    }
+
+    command_path[0] = '\0'; // Set an empty string if the command is not found
+    free(copied_full_path);
 }
