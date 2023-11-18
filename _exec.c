b55b2ded93767dcb9e56d13a0d487abe0caa712d
shaimaa <shaimafikry@yahoo.com>
2023-11-18 20:39:02 +0200

    edit

--- _exec.c
+++ _exec.c
@@ -8,7 +8,7 @@
 void _exec(char *command, char *filename, char *args[1024])
 {
 	char *command_path;
-
+	int status;
 	command_path = malloc(1024);
 	if (command_path == NULL)
 	{
@@ -26,11 +26,16 @@ void _exec(char *command, char *filename, char *args[1024])
 		else if (child_pid == 0)
 		{
 			if (execve(command_path, args, environ) == -1)
-				free(command_path), perror(filename), exit(2); }
+				free(command_path), perror(filename), exit(EXIT_FAILURE); }
 		else
-			wait(NULL); }
+		{
+			wait(&status);
+			if (WIFEXITED(status) == 0)
+				exit(WEXITSTATUS(status));
+		}
+	}
 	else
-		perror(filename), exit(2);
+		perror(filename);
 
 	free(command_path);
 }
